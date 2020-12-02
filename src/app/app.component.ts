37ec3ed3a409ab2cfa6332b10eb79b34fdf78572
MarnusCoetzee <marnuscoe@gmail.com>
2020-12-02 12:56:07 +0200

    finished static pages + 404 not found

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import * as fromDictionaries from './store/dictionaries';
 import * as fromRoot from './store';
+import * as fromUser from './store/user';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  isAuthorized$: Observable<boolean>;
   constructor(private store: Store<fromRoot.State>) {}
   ngOnInit(): void {
+    this.isAuthorized$ = this.store.pipe(select(fromUser.getIsAuthorized));
+
+    this.store.dispatch(new fromUser.Init());
     this.store.dispatch(new fromDictionaries.Read());
+  }
+
+  onSignOut(): void {
+    this.store.dispatch(new fromUser.SignOut());
   }
 }
--- src/app/pages/auth/pages/login/login.module.ts
+++ src/app/pages/auth/pages/login/login.module.ts
@@ -3,13 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  FormFieldModule,
+  InputModule,
+  PasswordModule,
+} from 'src/app/shared/controls';
+import { SpinnerModule } from 'src/app/shared/indicators';
+import { ButtonModule } from 'src/app/shared/buttons';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
-  ]
+    LoginRoutingModule,
+    ReactiveFormsModule,
+    FormFieldModule,
+    InputModule,
+    PasswordModule,
+    ButtonModule,
+    SpinnerModule,
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}
