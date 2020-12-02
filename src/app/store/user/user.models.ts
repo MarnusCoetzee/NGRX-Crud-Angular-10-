d817ef0fc38c1ca7f44e6ec87b3e0d4e549d7cde
MarnusCoetzee <marnuscoe@gmail.com>
2020-12-02 09:45:44 +0200

    finished user auth store

--- src/app/store/user/user.models.ts
+++ src/app/store/user/user.models.ts
@@ -1,4 +1,4 @@
-import { User, Recruiter, Employee } from '../../../models/backend';
+export { User, Recruiter, Employee } from '../../../models/backend';
 
 // requests models
 export interface EmailPasswordCredentials {
